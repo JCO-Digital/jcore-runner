81703847ce268bb55afa9d835950204eecfd5409
Niklas Schönberg <niklas@uplink.fi>
2024-01-12 22:27:54 +0200

    2.0.0

--- jcore-runner.php
+++ jcore-runner.php
@@ -4,7 +4,7 @@
  * Description: A WordPress plugin to easily allow manual running of scripts for maintenance and utility.
  * Plugin URI: https://github.com/JCO-Digital/jcore-runner#readme
  * Author: JCO Digital
- * Version: 1.1.2
+ * Version: 2.0.0
  * Author URI: http://jco.fi
  *
  * @package Jcore\Runner
