96fbdcfa2d33edb8476117cb2decd11be6df2a78
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-03-18 13:03:45 +0200

    chore: release version 2.1.1

--- jcore-runner.php
+++ jcore-runner.php
@@ -4,7 +4,7 @@
  * Description: A WordPress plugin to easily allow manual running of scripts for maintenance and utility.
  * Plugin URI: https://github.com/JCO-Digital/jcore-runner#readme
  * Author: JCO Digital
- * Version: 2.1.0
+ * Version: 2.1.1
  * Author URI: http://jco.fi
  * Text Domain: jcore-runner
  * Domain Path: /languages
