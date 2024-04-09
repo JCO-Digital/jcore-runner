5fcf6f001330671994dd3564c32839bf287f1ee5
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-04-09 16:58:07 +0300

    3.0.3

--- jcore-runner.php
+++ jcore-runner.php
@@ -4,7 +4,7 @@
  * Description: A WordPress plugin to easily allow manual running of scripts for maintenance and utility.
  * Plugin URI: https://github.com/JCO-Digital/jcore-runner#readme
  * Author: JCO Digital
- * Version: 3.0.2
+ * Version: 3.0.3
  * Author URI: http://jco.fi
  * Text Domain: jcore-runner
  * Domain Path: /languages
